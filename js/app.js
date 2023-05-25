f8ae0d01347aafb66fcfac81ce35992d746d779a
Apple <apple@Apples-iMac-2.local>
2023-05-25 14:03:33 +0530

    Add task modal update

--- js/app.js
+++ js/app.js
@@ -13,22 +13,37 @@ function check(){
 
 // A function to change text if there are 0 tasks.
 function noUpcomingTasks(){
-	
 	console.log(task.style)
 }
 
-// Adding Tasks
-var addTaskBtn = document.getElementById("add-task-btn");
+//Add Tasks
+var addTaskBtn = document.getElementById("add-task-btn-modal");
 addTaskBtn.addEventListener('click', function(e){
-	var taskName = prompt("Enter task: ");
-	var taskTime = prompt("Enter time: ");
 	var content = document.getElementById("task-box");
-	content.innerHTML = content.innerHTML + "<div class='row task-items'><div class='col-sm-8'><h4 class='task-name'><i class='icofont-tasks-alt'></i>" + taskName + "</h4><h5 class='task-time'><i class='icofont-clock-time'></i>" + taskTime +"</h5></div><div class='task-finish-btn col-sm-4'><button onclick='vanish(this)'class='finish-btn'>Finish Task</button></div></div>"
+	var taskDesc = document.querySelectorAll("#task-form")
+	console.log(taskDesc[0].value);
+	content.innerHTML = content.innerHTML + "<div class='row task-items'><div class='col-sm-8'><h4 class='task-name'><i class='icofont-tasks-alt'></i>" + taskDesc[0].value + "</h4><h5 class='task-time'><i class='icofont-clock-time'></i>" + taskDesc[1].value +"</h5></div><div class='task-finish-btn col-sm-4'><button onclick='vanish(this)'class='finish-btn'>Finish Task</button></div></div>"
 	check();
 })
+
 
 // Removing Tasks
 function vanish(c){
 	confirm("Have you really finished your task?")?c.parentElement.parentElement.remove():false;
 	check();
 }
+
+// Open Modal
+var openModal = document.getElementById("add-task-btn");
+openModal.addEventListener('click', function(e){
+	var taskModal = document.getElementById("task-modal");
+	taskModal.style.display = "flex";
+})
+
+
+//Closing Modal
+var closeModalBtn = document.getElementById("close-task-btn-modal");
+closeModalBtn.addEventListener('click', function(event){
+	var modal = document.getElementById("task-modal");
+	modal.style.display = "none";
+})
